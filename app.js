1d6458d216a38778db9a3b70fbeb815527921261
rdelva <roseldelva@gmail.com>
2022-08-27 19:53:39 -0400

    Set up authentication and console log to let the user know connection was sucessful

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const sequelize = require('sequelize');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -12,6 +13,18 @@ const app = express();
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
+
+
+(async ()=> {
+  
+  //Test the connection to the database
+  await sequelize.authenticate();
+  console.log('Testing the connection to the database');
+
+  
+  
+})();
+
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
