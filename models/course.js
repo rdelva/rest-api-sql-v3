1d207f6f3ae6413e542dca6b681a6a78aee61dce
rdelva <roseldelva@gmail.com>
2022-08-27 20:54:04 -0400

    added course.js and created course model

---
+++ models/course.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = () => {
+    class Course extends Sequelize.Model {}
+    Course.init({
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,           
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull:false,
+        },
+        description: {
+            type: Sequelize.TEXT,
+            estimatedTime: Sequelize.STRING,
+            allowNull: false,
+        },
+        materialsNeeded: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        }
+
+    }, {sequelize});
+}
