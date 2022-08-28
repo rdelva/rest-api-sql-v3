45d151d4afcc5dac99f146ac965796540c3d73ff
rdelva <roseldelva@gmail.com>
2022-08-28 10:53:27 -0400

    updating routes

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const app = require('./app');
 
 const router = express.Router();
 const  User  = require('./models').User;
@@ -21,8 +20,7 @@ function asyncHandler(cb){
 
 router.get('/users', asyncHandler( async (req, res) => {
 
-    const users = await User.findAll({});
-
+    const users = await User.findAll();
     res.json(users);
   }));
 
