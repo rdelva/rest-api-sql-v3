5e239e915929a8552161658f8c1c471c33a1d1eb
rdelva <roseldelva@gmail.com>
2023-02-20 23:02:41 -0500

    installed basic-auth

---
+++ middleware/auth-user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+
+// Middleware to authenticate the request using Basic Authentication.
+exports.authenticateUser = async (req, res, next) => {
+    let message; // store the message to display
+
+    // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+    
+    if(credentials) {
+        const user = await User.findOne({ where: {username: credentials.name} });
+        
+        if (user) {
+            const authenticated = bcrypt
+                .compareSync(credentials.pass, user.confirmedPassword);        
+            if (authenticated) { // If the passwords match
+                console.log(`Authentication successful for username: ${user.username}`);
+    
+                // Store the user on the Request object.
+                req.currentUser = user;
+            } else {
+                message = `User not found for username: ${credentials.name}`;
+            }
+        } else {
+            message = 'Auth header not found';
+        }
+
+    }
+
+
+    if (message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+      } else {
+        next();
+      }
+    
+    
+    next();
+}
