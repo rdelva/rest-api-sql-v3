ba7ec9ec54805c02cfcc77c5faa9d6a17e29e939
rdelva <roseldelva@gmail.com>
2023-02-20 22:24:41 -0500

    update POST user route to write into database

--- routes.js
+++ routes.js
@@ -36,13 +36,13 @@ router.post('/users', async (req, res) =>{
     
     // if(req.body.firstName && req.body.lastName && req.body.emailAddress && req.body.password){
        
-    //     const  user = await User.create({
+        // const  user = await User.create({
             
-    //         firstName: req.body.firstName,
-    //         lastName: req.body.lastName,
-    //         emailAddress: req.body.emailAddress,
-    //         password: req.body.password
-    //     });              
+        //     firstName: req.body.firstName,
+        //     lastName: req.body.lastName,
+        //     emailAddress: req.body.emailAddress,
+        //     password: req.body.password
+        // });              
         // res.setHeader('location', '/');
         // res.status(201).json(user).end();
 
@@ -51,12 +51,13 @@ router.post('/users', async (req, res) =>{
     // } 
 
     const user = req.body;
+    console.log(user);
     const errors = [];
 
     //Validate thane we have a name value
 
     if(!user.firstName){
-        errors.push('Please provide a value for "name"');
+        errors.push('Please provide a value for "first name"');
     }
 
     // Validate that we have an `last name` value.
@@ -70,28 +71,34 @@ router.post('/users', async (req, res) =>{
         errors.push('Please provide a value for "email"');
     }
 
-    // Validate that we have an `email` value.
-
- // Validate that we have a `password` value.
+    // Validate that we have a `password` value.
     let password = user.password;
     if (!password) {
-    errors.push('Please provide a value for "password"');
+        errors.push('Please provide a value for "password"');
     } else if (password.length < 8 || password.length > 20) {
-    errors.push('Your password should be between 8 and 20 characters');
+        errors.push('Your password should be between 8 and 20 characters');
     } else {
-    user.password = bcrypt.hashSync(password, 10);
+        user.password = bcrypt.hashSync(password, 10);
+        console.log(user.password);
     }
 
      // If there are any errors...
- if (errors.length > 0) {
-    // Return the validation errors to the client.
-    res.status(400).json({ errors });
-  } else {
+    if (errors.length > 0) {
+        // Return the validation errors to the client.
+        res.status(400).json({ errors });
+    } else { 
+    
+        // Set the status to 201 Created and end the response.
+        await User.create({
+            
+            firstName: user.firstName,
+            lastName: user.lastName,
+            emailAddress: user.emailAddress,
+            password:user.password
+        }); 
         // add the user profile and set location to '/'
         res.setHeader('location', '/');
-        
-        // Set the status to 201 Created and end the response.
- 
+        res.status(201).json(user).end();       
    }
 
     
