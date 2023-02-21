2507fbde7b304d098c832e6125cb40f17a87308c
rdelva <roseldelva@gmail.com>
2023-02-20 21:39:39 -0500

    add b crypt and add valdation to  POST User

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const {User, Course} = require('./models');
 
 
@@ -33,21 +34,66 @@ router.get('/users', asyncHandler( async (req, res) => {
 //Send a POST request that will create a new user
 router.post('/users', async (req, res) =>{
     
-    if(req.body.firstName && req.body.lastName && req.body.emailAddress && req.body.password){
+    // if(req.body.firstName && req.body.lastName && req.body.emailAddress && req.body.password){
        
-        const  user = await User.create({
+    //     const  user = await User.create({
             
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            emailAddress: req.body.emailAddress,
-            password: req.body.password
-        });              
-        res.setHeader('location', '/');
-        res.status(201).json(user).end();
+    //         firstName: req.body.firstName,
+    //         lastName: req.body.lastName,
+    //         emailAddress: req.body.emailAddress,
+    //         password: req.body.password
+    //     });              
+        // res.setHeader('location', '/');
+        // res.status(201).json(user).end();
 
+    // } else {
+    //     res.status(400).json({message: "First Name, Last Name, Email Address, Password is required"});
+    // } 
+
+    const user = req.body;
+    const errors = [];
+
+    //Validate thane we have a name value
+
+    if(!user.firstName){
+        errors.push('Please provide a value for "name"');
+    }
+
+    // Validate that we have an `last name` value.
+    if (!user.lastName) {
+        errors.push('Please provide a value for "last name"');
+    }
+
+    
+    // Validate that we have an `email` value.
+    if (!user.emailAddress) {
+        errors.push('Please provide a value for "email"');
+    }
+
+    // Validate that we have an `email` value.
+
+ // Validate that we have a `password` value.
+    let password = user.password;
+    if (!password) {
+    errors.push('Please provide a value for "password"');
+    } else if (password.length < 8 || password.length > 20) {
+    errors.push('Your password should be between 8 and 20 characters');
     } else {
-        res.status(400).json({message: "First Name, Last Name, Email Address, Password is required"});
-    } 
+    user.password = bcrypt.hashSync(password, 10);
+    }
+
+     // If there are any errors...
+ if (errors.length > 0) {
+    // Return the validation errors to the client.
+    res.status(400).json({ errors });
+  } else {
+        // add the user profile and set location to '/'
+        res.setHeader('location', '/');
+        
+        // Set the status to 201 Created and end the response.
+ 
+   }
+
     
 });
 
